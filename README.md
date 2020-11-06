483ab46f5da9050326c62f937bfadbcc4cbf98e1
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-11-06 13:54:13 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# issue-validator
+# Issue Validator
+
+This github action checks whether an issue is valid in terms of containing specified sections for certain labels.
+
+You can assign required sections for labels in workflow config.
+
+This action is designed to be used with following event types:
+
+```yml
+types: [edited, labeled, unlabeled]
+```
+
+You can find the full list of event types [here](https://docs.github.com/en/free-pro-team@latest/actions/reference/events-that-trigger-workflows#issues)
+
+## Inputs
+
+### `required-sections`
+
+**Required** a list of labels and sections assigned to them. Labels should be separated by `;`, sections by `,`. First element of every list is a label name, all of the following are required sections names.
+
+It should look like this:
+
+- `bug,Description,Steps to reproduce,Expected behaviour;feature request,motivation`
+- `documentation,description,motivation;bug,description,expected behavior,actual behavior;question,description`.
+
+This is case insensitive but watch out for the spaces(just be careful to not put a space after separators, that string will be just split by them without trimming any spaces as they may occur in section names).
+
+## Example usage
+
+Here is the example config file:
+
+```yml
+name: Check versions
+on:
+  issues:
+    types: [edited, labeled, unlabeled]
+
+jobs:
+  check_issue:
+    runs-on: ubuntu-latest
+    name: Validate issue
+    steps:
+      - name: Validate issue
+        id: validate-issue
+        uses: karol-bisztyga/issue-validator@v1
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+          required-sections: documentation,description;bug,description,expected behavior,actual behavior;question,description
+```
---
+++ index.js
@@ -0,0 +1,158 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+/**
+ * should be used with types: [edited, labeled, unlabeled]
+ */
+
+(async () => {
+  try {
+    const { issue, payload } = github.context;
+
+    const issueLabels = payload.issue.labels.map((label) => {
+      return label.name;
+    });
+
+    const client = github.getOctokit(
+      core.getInput("github-token", { required: true })
+    );
+
+    let labeledWith = undefined;
+    if (payload.action === "labeled") {
+      labeledWith = payload.label.name;
+    }
+
+    const labels = core.getInput("required-sections").toLowerCase().split(";");
+    const requiredSections = [];
+    labels.forEach((labelData) => {
+      const arr = labelData.split(",");
+      const labelName = arr[0];
+      // check label name - if it's undefined, the action is [edited] and we should check all the labels
+      // otherwise focus only on label which has been added - which means skip all the rest
+      if (labeledWith) {
+        // [labeled]
+        if (labeledWith !== labelName) {
+          return;
+        }
+      } else {
+        // [edited]
+        // don't check if this label is not assigned to the issue
+        if (issueLabels.indexOf(labelName) === -1) {
+          return;
+        }
+      }
+      const sections = arr.slice(1);
+      sections.forEach((section) => {
+        requiredSections.push([section, labelName]);
+      });
+    });
+
+    // there are no required sections, we should break here
+    // unless it comes from the fact that the action is [unlabeled]
+    // then we should inform the user that the issue is correct
+    if (!requiredSections.length && payload.action !== "unlabeled") {
+      console.log("nothing to do");
+      return;
+    }
+
+    const issueBody = payload.issue.body.toLowerCase();
+    const problems = [];
+
+    const checkIfSectionExists = (section) => {
+      const regexp = new RegExp(`[#]+[ ]{0,1}${section}`);
+      const position = issueBody.search(regexp);
+      return position;
+    };
+
+    const checkIfSectionNotEmpty = (section, sectionPosition) => {
+      const sub = issueBody.substr(sectionPosition);
+      const sectionStartIndex =
+        sub.search(new RegExp(`${section}`)) + section.length;
+      const nextSectionPos = sub.search(new RegExp("\n[#]+"));
+      const end = nextSectionPos === -1 ? undefined : nextSectionPos;
+      const sectionContent = sub.substring(sectionStartIndex, end);
+      return sectionContent.replace(/\r?\n|\r/g, "").replace(/ /g, "").length;
+    };
+
+    const checkSection = (section) => {
+      const sectionPosition = checkIfSectionExists(section);
+      if (sectionPosition !== -1) {
+        if (!checkIfSectionNotEmpty(section, sectionPosition)) {
+          return `Section ${section} seems to be empty`;
+        }
+      } else {
+        return `Section required but not found: ${section}`;
+      }
+    };
+
+    requiredSections.forEach(([section, label]) => {
+      const problem = checkSection(section);
+      problem && problems.push(problem + `(for label ${label})`);
+    });
+
+    const updateComment = async (body) => {
+      let result = false;
+      const comments = await client.issues.listComments({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+      });
+      if (comments && comments.data && comments.data.length) {
+        const lastComment = comments.data.slice(-1)[0];
+        if (lastComment && lastComment.user.login === "github-actions[bot]") {
+          console.log("updated comment");
+          body = `${lastComment.body}\n\n### UPDATE:\n${body}`;
+          result = true;
+          await client.issues.updateComment({
+            owner: issue.owner,
+            repo: issue.repo,
+            issue_number: issue.number,
+            comment_id: lastComment.id,
+            body,
+          });
+        }
+      }
+      return result;
+    };
+
+    const createNewComment = async (body) => {
+      console.log("creating new comment");
+      await client.issues.createComment({
+        owner: issue.owner,
+        repo: issue.repo,
+        issue_number: issue.number,
+        body,
+      });
+    };
+
+    const createOrUpdateComment = async (header, body) => {
+      // in case last comment of this issue is created by bot
+      // we don't want to re-post but update instead
+      let lastCommentUpdated = await updateComment(body);
+
+      // no bot-created last comment detected, creating new comment
+      if (!lastCommentUpdated) {
+        await createNewComment(header + body);
+      }
+    };
+
+    const validatorHeader = "## Issue validator\n\nHello!\n";
+    if (problems.length) {
+      const header =
+        "It seems like there are some problems with your issue. Please fix them and wait for the validator to confirm that everything is alright.\nThank you!\n\nValidator encountered following problems:\n\n";
+      createOrUpdateComment(
+        validatorHeader + header,
+        problems.map((problem) => `- ${problem}\n`).join("")
+      );
+    } else {
+      console.log("everything is ok");
+      createOrUpdateComment(
+        validatorHeader,
+        "Congratulations! Your issue passed the validator! Thank you!"
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    core.setFailed(error.message);
+  }
+})();
